6d7380c660f29bff9197f215f4a8da7c512e4f9d
Klazen <cmurphy1337@live.com>
2015-09-29 17:43:33 -0400

    Adding website config
    added website config file to genericize website details

---
+++ inc/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Include this file in dbinc.php
+ * Make sure to set your own values here!
+ */
+define('GMF_CONFIG_SET',true);
+define('GMF_HOST_NAME',''); //hostname (with protocol) for the website where this is installed, for example http://example.com
+define('GMF_PATH',''); //root path (with trailing slash) where this is installed, for example / for root or /gmf/ for the gmf subdirectory
+?>
